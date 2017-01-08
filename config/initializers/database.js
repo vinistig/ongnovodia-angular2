83749ebfb8486946f3ca2d57109a03933415c139
Vinicius Stigliani <vinistig@br.ibm.com>
2017-01-08 21:06:59 -0200

    fix database connection

--- config/initializers/database.js
+++ config/initializers/database.js
@@ -1,7 +1,7 @@
 function bootDatabase(config) {
 	const mongoose = require('mongoose')
 	mongoose.Promise = global.Promise
-	mongoose.connect(config.uri + config.name)
+	mongoose.connect(config.uri)
 }
 
 module.exports = bootDatabase;
